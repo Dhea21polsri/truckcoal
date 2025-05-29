597163a5828434233d0e4b022f353c9f3b19eb48
Dhea21polsri <156050411+Dhea21polsri@users.noreply.github.com>
2025-05-29 19:05:10 +0700

    kedua

---
+++ lib/supervisor/tambahuser.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TambahUser extends StatefulWidget {
+  const TambahUser({super.key});
+
+  @override
+  State<TambahUser> createState() => _TambahUserState();
+}
+
+class _TambahUserState extends State<TambahUser> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
